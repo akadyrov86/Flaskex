53b1a0f403f9cb956c2f2275a7058846c3a9b75e
AKMAL KADYROV <AKMALKADYROV@AKMALs-MacBook-Pro.local>
2019-03-20 18:58:23 -0600

    flaskex service added

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -7,14 +7,23 @@ node {
     stage("git clone"){
         git 'https://github.com/akadyrov86/Flaskex.git'
     }
+    stage("Run app"){
+        sh "ssh ec2-user@${IP}  sudo mkdir /flaskex 2> /dev/null"
+    }
     stage("Copy files"){
         sh "scp  -r *  ec2-user@${IP}:/tmp/"
+    }
+    stage("Move files to /fleskex"){
+        sh "ssh ec2-user@${IP}  sudo mv /home/ec2-user/*  /flaskex/"
     }
     stage("Install requirment"){
          sh "ssh ec2-user@${IP}   sudo pip install -r /tmp/requirements.txt"
         
     }
-    stage("Run app"){
-        sh "ssh  ec2-user@${IP}   python  /tmp/app.py"
+    stage("move service to /etc"){
+        sh "ssh ec2-user@${IP} sudo /flaskex/flaskex.service  /etc/systemd/system"
+    }
+    stage("Start service"){
+        sh "ssh ec2-user@${IP}  sudo systemctl start flaskex"
     }
 } 
